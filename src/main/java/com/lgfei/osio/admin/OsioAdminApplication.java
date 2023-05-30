82f035f1e7f97798e8756cdec426209fd1eb31b0
long.gf <long.gf@topsports.com.cn>
2023-05-30 10:39:37 +0800

    init by https://start.aliyun.com/

---
+++ src/main/java/com/lgfei/osio/admin/OsioAdminApplication.java
@@ -0,0 +1,13 @@
+package com.lgfei.osio.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsioAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsioAdminApplication.class, args);
+    }
+
+}
