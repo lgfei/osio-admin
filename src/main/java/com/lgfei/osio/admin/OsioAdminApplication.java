b40ac9a65029f7ac1b475ccce196646617d6a040
long.gf <long.gf@topsports.com.cn>
2023-05-30 15:17:01 +0800

    add bootstrap.yml

--- src/main/java/com/lgfei/osio/admin/OsioAdminApplication.java
+++ src/main/java/com/lgfei/osio/admin/OsioAdminApplication.java
@@ -1,8 +1,13 @@
 package com.lgfei.osio.admin;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAdminServer
 @SpringBootApplication
 public class OsioAdminApplication {
 
